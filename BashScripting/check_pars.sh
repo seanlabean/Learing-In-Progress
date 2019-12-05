0b6004a0eb2f62d87086c5511e81382e2f55604c
Sean Lewis <scl63@drexel.edu>
2019-12-05 17:48:29 -0500

    Final changes to check_pars.sh, functions properly. Added new directory test-empty, meant to remain empty until check_pars.sh is run.

--- BashScripting/check_pars.sh
+++ BashScripting/check_pars.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+# 
+# This script is purpose built to be run in a TORCH "simulation" directory.
+# What this script does (two options):
+# 1. If simulation is new (output directory defined in flash.par is empty),
+#    copys flash.par file to the data directory and exits.
+# 2. If simulation is a RESTART (output directory is NOT empty),
+#    runs a diff between current and old flash.par files so user can confirm
+#    that only intended changes have been made to the parameter file.
+#
+# - Sean C. Lewis; Drexel University (Dec. 2019)
+#
+
 unset FILE
 unset line
 unset words
@@ -6,37 +19,64 @@ unset output
 unset restart
 
 FILE=./flash.par
-output=output_dir
 line=''
-#restart=False
+
+# Check if input FILE exists. https://linuxize.com/post/bash-check-if-file-exists/
 if [ ! -f "$FILE" ]; then
-    echo "$FILE does not exist."
+    echo "ERROR: $FILE does not exist."
     echo "Maybe you moved it somewhere?"
+    echo "Exiting..."
+    # Exit .sh script without terminating shell. https://stackoverflow.com/questions/11141120
+    kill -INT $$
 else 
-    echo "$FILE exists."
-    echo "Checking if a Torch restart."
+    echo ""
 fi
 
+# Read in lines from FILE, divide each line into array of words.
+# https://stackoverflow.com/questions/10929453
 while read -a words; do
-    if [[ ${words[0]} == "restart" ]]; then
-	echo "You are issuing a RESTART."
-    fi
-done < "$FILE"
-
-
-while read -a words; do
-    if [[ ${words[0]} == "restart" ]]; then
-	echo "You are issuing a RESTART."
-    fi
-    
     if [[ ${words[0]} == "output_dir" ]]; then
 	echo "output directory is ${words[2]}"
-	echo "Checking"
+	output=${words[2]}
+	echo "Checking status of $output"
     fi
 done < "$FILE"
 
+# Check if output directory is empty. https://superuser.com/questions/352289
+if [ -n "$(find "$output" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
+    echo "Empty directory, we need to copy over .par file."
+    # Prompt user for confirmation. https://stackoverflow.com/questions/226703
+    while true; do
+	read -p "This is a new simulation run, correct? (y/n): " yn
+	case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) kill -INT $$;;
+            * ) echo "Please answer y or n.";;
+	esac
+    done
+    echo "Copying $FILE to $output."
+    cp $FILE $output
+    echo "Simulation commencing..."
+else
+    echo "$output is NOT empty."
+    echo "Therefore, RESTART is assumed. Performing .par diff check."
+    diff $FILE $output/$FILE
+    while true; do
+	read -p "Check these differences, is everything expected? (y/n): " yn
+	case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) kill -INT $$;;
+            * ) echo "Please answer y or n.";;
+	esac
+    done
+    echo "Simulation RESTART initiated..."
+fi
+
+# I am not sure if the unsetting of the variables defined within this script is entirely
+# necessary, but I like it because it leaves my shell's env vars less cluttered. - SCL
 unset FILE
 unset line
 unset words
 unset output
 unset restart
+unset yn
